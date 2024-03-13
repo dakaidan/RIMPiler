2866e4d61b1ce19c1f077126aa9b592e42e9a6b2
Aidan Dakhama <ai.dakhama@gmail.com>
2024-03-13 10:52:46 +0000

    Updated report

--- Documentation/Report.sh
+++ Documentation/Report.sh
@@ -2,4 +2,4 @@ SCRIPT="$0"
 SCRIPT_DIR="$( cd "$( dirname "${SCRIPT}" )" && pwd )"
 
 cd "$SCRIPT_DIR"/Report || exit
-latexmk -pvc -pdf -jobname=Report main.tex
+latexmk -pvc -pdf -jobname=Report report.tex
